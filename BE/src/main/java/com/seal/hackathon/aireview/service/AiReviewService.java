6441b9ee20e906ed60274a32cbb285fcfa649dcb
lamthanhphuc <phucltse184678@fpt.edu.vn>
2026-06-09 10:27:19 +0700

    BE ai review: remove unused scheduler health skeleton

--- BE/src/main/java/com/seal/hackathon/aireview/service/AiReviewService.java
+++ BE/src/main/java/com/seal/hackathon/aireview/service/AiReviewService.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class AiReviewService {
 
-    public String skeletonStatus() {
-        return "AiReviewService skeleton ready (advisory only, no ranking impact)";
-    }
-
     public AiReviewRunSummary runScheduledReview() {
         return new AiReviewRunSummary(0, "SKELETON_ONLY");
     }
